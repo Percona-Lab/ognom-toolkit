8f2c9eabe7217b64fbea22c01231f3b93756ac8d
Fernando Ipar <fipar@acm.org>
2015-09-16 12:18:04 -0300

    added lab scripts directory

---
+++ lab/README.md
@@ -0,0 +1,1 @@
+These scripts are meant to test different mongodb topologies and should not be used in production
---
+++ lab/common.sh
@@ -0,0 +1,53 @@
+init_dbpath()
+{
+    [ $# -eq 1 ] || {
+	cat <<EOF>&2
+    usage: init_dbpath <directory>
+    Initializes (creates if needed, removes data if any exists) a directory for use as an argument to --dbpath.
+EOF
+    }
+    test -d $1 || mkdir -p $1
+    rm -rf $1/*
+    touch $1/mongod.pid
+    chmod a+w $1/mongod.pid
+}
+
+start_mongod()
+{
+    [ $# -ge 2 ] || {
+	cat <<EOF>&2
+    usage: start_mongod <dbpath> <port> [replSet|configsvr]
+    Starts a new mongod instance with the provided args for --dbpath and --port (and optionally indicates a replica set name, or starts the instance as a config server). 
+    Initializes the datadir if needed. 
+EOF
+    }
+    dbpath=$1; port=$2; replSet=$3
+    [ -n "$replSet" ] && {
+	[ "$replSet" == "configsvr" ] && replSet="--configsvr" || replSet="--replSet $replSet"
+    }
+    mongod --dbpath $dbpath --port $port --logpath $dbpath/mongod.log $replSet --pidfilepath $dbpath/mongod.pid --fork
+}
+
+start_mongos()
+{
+    [ $# -eq 3 ] || {
+	cat <<EOF>&2
+    usage: start_mongos <dpath> <port> <configdb>
+    Starts a new mongos instance with the specified arguments. 
+EOF
+    }
+    dbpath=$1; port=$2; configdb=#3
+    # we intentionally use mongod.pid below so we can use stop_mongo
+    mongos --setParameter enableTestCommands=1 --configdb $configdb --logpath $dbpath/mongos.log --port $port --pidfilepath $dbpath/mongod.pid --fork
+}
+
+stop_mongo()
+{
+    [ $# -eq 1 ] || {
+cat <<EOF>&2
+    usage: stop_mongo <dbpath>
+    Sends SIGTERM to the mongod/mongos instance running out of the specified datadir. 
+EOF
+    }
+    kill $(cat $1/mongod.pid)
+}
